dfc85642d5ff6227551d0566553076971cf33381
Adrian Sims <adriansims62781@gmail.com>
2020-03-31 16:06:32 +0200

    Add project files.

---
+++ IsProc64/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsProc64
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var processes = Process.GetProcesses();
+                foreach (var process in processes)
+                {
+                    var name = GetProcessFullName(process);
+                    Console.WriteLine($"{name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+
+        private static string GetProcessFullName(Process process)
+        {
+            try
+            {
+                var name = process.MainModule.FileName;
+                return name;
+            }
+            catch (Win32Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
